cab18e31d93ba5b79ccaa3f91811457849a01fde
Mehdi YAHIA CHERIF <mehdi@localhost.localdomain>
2023-04-15 01:52:32 +0100

    aligned basmala

--- src/lib/components/TafsirModal.svelte
+++ src/lib/components/TafsirModal.svelte
@@ -1,12 +1,14 @@
 
 <script>
+    import { surah } from "../store";
+    import { quran } from "../values/quran_summerizing";
     export let tafsir_nb;
     export let tafsir_txt;
 </script>
-<input type="checkbox" id="my-modal-4" class="modal-toggle" />
-<label for="my-modal-4" class="modal cursor-pointer">
+<input type="checkbox" id="tafsir-modal" class="modal-toggle" />
+<label for="tafsir-modal" class="modal cursor-pointer">
   <label class="modal-box relative" for="">
-    <h3 class="text-lg font-bold">{tafsir_nb}</h3>
+    <h3 class="text-lg font-bold">{quran[$surah+1].name_translations.ar}-{tafsir_nb}</h3>
     <p class="py-4">{tafsir_txt}</p>
   </label>
 </label>
