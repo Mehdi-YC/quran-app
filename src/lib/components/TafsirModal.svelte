0d01dac67192845f7f17a27cc8b0755e421a2cc4
mehdi <mehdidouy@gmail.com>
2024-02-07 22:06:12 +0100

    fix: minor off by one error

--- src/lib/components/TafsirModal.svelte
+++ src/lib/components/TafsirModal.svelte
@@ -10,7 +10,7 @@
 <input type="checkbox" id="tafsir-modal" class="modal-toggle" />
 <label for="tafsir-modal" class="modal cursor-pointer">
   <label class="modal-box relative" for="">
-    <h3 class="text-lg font-bold">{quran[$surah+1].name_translations.ar}-{tafsir_nb}</h3>
+    <h3 class="text-lg font-bold">{quran[$surah].name_translations.ar}-{tafsir_nb+1}</h3>
     <p class="py-4">{tafsir_txt}</p>
   </label>
 </label>
